74d5855c871129c342b7b977bac7e8960bcbcd49
josh <josh@zool.com>
2017-11-27 12:17:33 +1100

    parallel sensor request

--- as7263.go
+++ as7263.go
@@ -244,8 +244,7 @@ func retry(attempts int, sleep time.Duration, fn func() error) (err error) {
 	return fmt.Errorf("after %d attempts, last error: %s", attempts, err)
 }
 
-func (a *AS7263) ReadAll() (Spectrum, error) {
-	fmt.Println("readall")
+func (a *AS7263) Request() time.Duration {
 	if err := a.setConfig(); err != nil {
 		log.Println(err)
 	}
@@ -257,6 +256,12 @@ func (a *AS7263) ReadAll() (Spectrum, error) {
 	if err := a.setMode(3); err != nil {
 		log.Println(err)
 	}
+	return time.Millisecond * 1443
+}
+
+func (a *AS7263) ReadAll() (Spectrum, error) {
+	fmt.Println("readall")
+
 	ready, err := a.dataReady()
 	if err != nil {
 		log.Println(err)
